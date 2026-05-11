28cee1ade65075cc6bd416965a5dcc2a4c450645
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-11 22:55:01 +0530

    Added the UC1 Features

--- SLproblem/src/SnakeAndLadder.java
+++ SLproblem/src/SnakeAndLadder.java
@@ -1,6 +1,12 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the snake and ladder game");
+        System.out.println("Roll the dice");
+        Random random=new Random();
+        int dice = random.nextInt(6)+1;
+        System.out.println("Dice Rolled: "+dice);
     }
 }
